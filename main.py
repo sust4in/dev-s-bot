dfe4cba029e303b11e231aaeb5d5ce15f4741282
Fatih Melih Ersoy <fatihmelih9@gmail.com>
2024-02-07 22:58:14 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import nextcord
+from nextcord.ext import commands
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with open('config/config.json') as config_file:
+    config = json.load(config_file)
+
+intents = nextcord.Intents().all()
+
+bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is running!')
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py') and not filename.startswith('__'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(os.getenv('DISCORD_TOKEN'))
